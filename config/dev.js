688587b30d35d5bbc228776d88dfcb7d01b07303
wanli.yu <wanli.yu@beibei.com>
2019-06-19 23:30:32 +0800

    add yml

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://yuwanli:yuwanli919293@cluster0-shard-00-00-hcnmn.mongodb.net:27017,cluster0-shard-00-01-hcnmn.mongodb.net:27017,cluster0-shard-00-02-hcnmn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
